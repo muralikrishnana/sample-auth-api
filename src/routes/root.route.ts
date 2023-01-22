94ac89c2cef597964abbb3288d5e10ecfa055400
Murali Krishnan A <amuralikrishnan@outlook.com>
2023-01-22 15:07:19 +0530

    Add root get endpoint.

--- src/routes/root.route.ts
+++ src/routes/root.route.ts
@@ -3,6 +3,9 @@ import authRouter from "./auth/auth.route";
 
 const rootRouter = express.Router();
 
+rootRouter.get("/", (req, res) => {
+  res.send("Hello from Sample Auth API");
+});
 rootRouter.use("/auth", authRouter);
 
 export default rootRouter;
